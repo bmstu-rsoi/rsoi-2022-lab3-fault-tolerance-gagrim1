15f5da6398077ee895a3baf3d19c00be6b24d1fb
yuromanov <gaggy.rom@gmail.com>
2023-01-12 01:06:45 +0300

    feat: init code

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -3,9 +3,13 @@
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 path=$(dirname "$0")
 
+echo "Sleeping for 90 seconds"
+sleep 90
+echo "Woke up!"
+
 PIDs=()
 for port in "${PORTS[@]}"; do
-  "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
+  "$(pwd)"/scripts/wait-for.sh -t 120 "localhost:$port" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
 
